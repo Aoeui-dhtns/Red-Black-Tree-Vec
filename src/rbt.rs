241a5452c0489a3ba3252819c6da348621d23ff0
aoeui-dhtns <95533874+Aoeui-dhtns@users.noreply.github.com>
2021-12-16 23:18:07 -0500

    Add functionality to add the root. Begin work on in order traversal so that I can test

--- src/rbt.rs
+++ src/rbt.rs
@@ -9,12 +9,13 @@ use std::collections::LinkedList;
  *      doesn't grow monotonically in memory
  *      color: a vector of boolean values to denote the color (red or black) of the node
  */
-struct Tree<T> {
+pub struct Tree<T> {
    graph: Vec<Node<T>> ,
    edge_list: Vec<Vec<Option<usize>>>, // Parent, left child, right child
    empty: LinkedList<usize>, // Linked list so that we don't have to worry about allocation
     // Linked list has a is_empty function. That will be useful for insertions
     color: Vec<bool>,
+    root: Option<usize>, // index with the root of the tree
 }
 // Nodes simply contain the data
 struct Node<T> {
@@ -24,6 +25,43 @@ struct Node<T> {
 impl<T> Tree<T>  {
     /// Function to create a new Red-Black Tree. Returns an empty tree
     pub fn new() -> Tree<T> {
-        Tree { graph: Vec::new(), edge_list: Vec::new(), empty: LinkedList::new(), color: Vec::new()}
+        Tree { graph: Vec::new(), edge_list: Vec::new(), empty: LinkedList::new(), color: Vec::new(), root: None}
+    }
+    /// Insert does exactly what it says, it inserts data into the tree, rebalancing if necessary
+    pub fn insert(&mut self, input: T) {
+        if self.graph.len() == self.empty.len() { // Tree is empty, add the root
+            if !self.empty.is_empty() {
+
+            } else {
+                self.graph.push(Node { data: input});
+                self.edge_list.push(vec![None, None, None]); // The root has no parents, and no siblings. How sad :(
+            }
+        }
+    }
+
+    /// in_order traverses the tree and returns a list of the nodes in order
+    pub fn in_order(self) -> LinkedList<T>
+        where T: Copy,
+    {
+        let mut stack: LinkedList<T> = LinkedList::new();
+        if self.graph.len() == self.empty.len() { // Tree is empty
+           // Do nothing 
+        } else {
+            match self.root {
+                Some(r) => {
+                    let left_child = self.edge_list[r][1];
+                    let right_child = self.edge_list[r][2];
+                    stack.push_back(self.graph[r].data);
+                    self.in_order_recursive(left_child);
+                    self.in_order_recursive(right_child);
+                }
+                None => { unreachable!() }
+            }
+        }
+        stack
+    }
+    // Helper function to build the stack
+    fn in_order_recursive(&self, index: Option<usize>) -> LinkedList<T>{
+        LinkedList::new()
     }
 }
